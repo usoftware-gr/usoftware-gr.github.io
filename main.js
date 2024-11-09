b8eb21ac2adcf60e0292c42ab0d28c80cc86befc
umaidango <162565401+umaidango@users.noreply.github.com>
2024-11-09 20:27:26 +0900

    header ok

---
+++ main.js
@@ -0,0 +1,11 @@
+const header = document.querySelector('#header_content');
+const header_sc = document.querySelector('#header_sc');
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
+    header.style.display = 'none';
+    header_sc.style.display = 'block';
+  } else {
+    header.style.display = 'block';
+    header_sc.style.display = 'none';
+  }
+});
