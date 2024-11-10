6b3499623d94f36dda2e7802b10a85651ce6497d
umaidango <162565401+umaidango@users.noreply.github.com>
2024-11-10 16:43:22 +0900

    ff

--- main.js
+++ main.js
@@ -23,6 +23,15 @@ function dark_mode(){
   moon.style.display = "none";
   logo_img.src = "img/white_logo.svg";
   localStorage.setItem("color_mode", "dark");
+  menu_svg.innerHTML = '<img style="vertical-align: 38%;margin-left: 20px; height:28px;" src="img/menu_white.svg">';
+  document.getElementById("menu").style.background = "#242424";
+
+  const allLinks = document.querySelectorAll('a');
+
+// 各 a タグのスタイルを変更
+allLinks.forEach(link => {
+  link.style.color = 'white';
+});
   
 }
 
@@ -31,7 +40,6 @@ function light_mode(){
   const li = document.getElementsByTagName("li")[0];
   const sun = document.getElementById("sun"); // IDで取得する
   const moon = document.getElementById("moon"); // IDで取得する
-  
   body.style.background = "#fff";
   header_content.style.background = "#fff";
   sun.style.display = "none";
@@ -39,6 +47,46 @@ function light_mode(){
   document.body.style.color = "#000";
   logo_img.src = "img/logo.svg";
   localStorage.setItem("color_mode", "light");
+  menu_svg.innerHTML = '<img style="vertical-align: 38%;margin-left: 20px; height:28px;" src="img/menu.svg">';
+  document.getElementById("menu").style.background = "#fff";
+  const allLinks = document.querySelectorAll('a');
+
+// 各 a タグのスタイルを変更
+allLinks.forEach(link => {
+  link.style.color = '#000';
+});
+}
+
+link_content_menu = document.getElementById("link_content_menu");
+link_content_menu.innerHTML = '<ul style="list-style: none;/*! text-align: center; *//*! display: inline-block; */vertical-align: -20%;"><a href="/index.html"><li style="/*! float: left; */padding-right: 20px;">ホーム</li></a><a href="/about.html"><li style="/*! float: left; */padding-right: 20px;">U Softwareについて</li></a><a href="/products.html"><li style="/*! float: left; */padding-right: 20px;">製品</li></a><a href="https://ku-daa.web.app/"><li style="/*! float: left; */padding-right: 20px;">ソフトウェアラインナップ</li></a><a href="/blog/"><li style="/*! float: left; */padding-right: 20px;">ブログ</li></a><a href="http://umaidango.y.ribbon.to/bbs/"><li style="/*! float: left; */padding-right: 20px;">掲示板</li></a><a href="/character.html"><li style="padding-right: 20px;">キャラクター</li></a><a href="/kabegami/"><li style="/*! float: left; */padding-right: 20px;">壁紙</li></a><a href="/dic/"><li style="/*! float: left; */padding-right: 20px;">だんご大百科</li></a><a href="/hamster/"><li style="/*! float: left; */padding-right: 20px;">ハムスター日記</li></a><a href="/me/"><li style="/*! float: left; */padding-right: 20px;">うまいだんごのブログ</li></a><a href="/web-styles/"><li style="/*! float: left; */padding-right: 20px;">Web Styles</li></a><a href="/chipi-audio/"><li style="/*! float: left; */padding-right: 20px;">Chipi Audio</li></a><a href="https://share-tool.vercel.app/"><li style="/*! float: left; */padding-right: 20px;">共有ツール</li></a></ul>';
+
+
+function open_menu() {
+
+  if(menu.style.display == "none"){
+    menu.style.display = "block";
+    menu.style.animation = "0.3s updown-anime";
+
+  }
+  else if(menu.style.display == "block"){
+    menu.style.animation = "0.3s downup-anime";
+
+    setTimeout(() => {
+      menu.style.display = "none";
+    }, 180);
+
+  }
+  
 }
 
 
+function close_menu()
+{
+  menu.style.animation = "0.3s downup-anime";
+
+  setTimeout(() => {
+    menu.style.display = "none";
+  }, 280);
+  
+  
+}
