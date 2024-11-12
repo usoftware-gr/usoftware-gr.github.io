1f42bfdd5c0a5c7af22ead55a332b90947b6e965
umaidango <162565401+umaidango@users.noreply.github.com>
2024-11-12 20:33:21 +0900

    f

--- main.js
+++ main.js
@@ -15,13 +15,12 @@ function dark_mode(){
   const li = document.getElementsByTagName("li")[0];
   const sun = document.getElementById("sun"); // IDで取得する
   const moon = document.getElementById("moon"); // IDで取得する
-
+  logo_img.innerHTML = '<svg version="1.1" viewBox="0 0 250 250.00001" fill="none" stroke="none" stroke-linecap="square" stroke-miterlimit="10" sodipodi:docname="u_logo_svg.svg" inkscape:version="1.3.2 (091e20e, 2023-11-25, custom)" width="250" height="250" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" style="margin-left: 10px;height: 42px;width: 42px;"><defs id="defs18"></defs><sodipodi:namedview id="namedview18" pagecolor="#ffffff" bordercolor="#000000" borderopacity="0.25" inkscape:showpageshadow="2" inkscape:pageopacity="0.0" inkscape:pagecheckerboard="0" inkscape:deskcolor="#d1d1d1" inkscape:zoom="0.50000001" inkscape:cx="95.999997" inkscape:cy="298.99999" inkscape:window-width="1920" inkscape:window-height="1129" inkscape:window-x="-8" inkscape:window-y="-8" inkscape:window-maximized="1" inkscape:current-layer="svg18"></sodipodi:namedview><clipPath id="p.0"><path d="M 0,0 H 255.29396 V 250.58792 H 0 Z" clip-rule="nonzero" id="path1"></path></clipPath><path style="fill: #ffffff;fill-opacity:1;stroke-width:0.5" d="m 29.825172,225.375 c 0.257864,-1.03125 9.533851,-42.20616 20.613305,-91.49981 11.079455,-49.293635 20.238301,-89.708141 20.352993,-89.810006 0.306169,-0.271927 51.49517,-6.946687 51.69179,-6.74032 0.0918,0.09633 -6.5622,30.437636 -14.7866,67.425136 -8.2244,36.9875 -14.837525,67.55459 -14.69584,67.92687 0.141685,0.37229 0.588255,0.55 0.99238,0.39492 0.40412,-0.15507 11.14597,-1.1063 23.87078,-2.11384 12.72481,-1.00754 23.19583,-1.88256 23.26892,-1.94449 0.0731,-0.0619 7.0308,-31.82267 15.46155,-70.5794 l 15.32865,-70.466794 26.7352,-5.620213 c 14.70436,-3.091117 26.82424,-5.531179 26.93306,-5.422359 0.1684,0.168398 -46.32785,199.271566 -46.64541,199.741906 -0.0824,0.12206 -147.274425,10.5543 -149.267785,10.57935 -0.177009,0.002 -0.110856,-0.8397 0.147007,-1.87095 z" id="path43"></path></svg>';
   body.style.background = "#333";
   header_content.style.background = "#333";
   document.body.style.color = "#fff";
   sun.style.display = "block";
   moon.style.display = "none";
-  logo_img.innerHTML = '<svg version="1.1" viewBox="0 0 250 250.00001" fill="none" stroke="none" stroke-linecap="square" stroke-miterlimit="10" sodipodi:docname="u_logo_svg.svg" inkscape:version="1.3.2 (091e20e, 2023-11-25, custom)" width="250" height="250" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" style="margin-left: 10px;height: 42px;width: 42px;"><defs id="defs18"></defs><sodipodi:namedview id="namedview18" pagecolor="#ffffff" bordercolor="#000000" borderopacity="0.25" inkscape:showpageshadow="2" inkscape:pageopacity="0.0" inkscape:pagecheckerboard="0" inkscape:deskcolor="#d1d1d1" inkscape:zoom="0.50000001" inkscape:cx="95.999997" inkscape:cy="298.99999" inkscape:window-width="1920" inkscape:window-height="1129" inkscape:window-x="-8" inkscape:window-y="-8" inkscape:window-maximized="1" inkscape:current-layer="svg18"></sodipodi:namedview><clipPath id="p.0"><path d="M 0,0 H 255.29396 V 250.58792 H 0 Z" clip-rule="nonzero" id="path1"></path></clipPath><path style="fill:#fff;fill-opacity:1;stroke-width:0.5" d="m 29.825172,225.375 c 0.257864,-1.03125 9.533851,-42.20616 20.613305,-91.49981 11.079455,-49.293635 20.238301,-89.708141 20.352993,-89.810006 0.306169,-0.271927 51.49517,-6.946687 51.69179,-6.74032 0.0918,0.09633 -6.5622,30.437636 -14.7866,67.425136 -8.2244,36.9875 -14.837525,67.55459 -14.69584,67.92687 0.141685,0.37229 0.588255,0.55 0.99238,0.39492 0.40412,-0.15507 11.14597,-1.1063 23.87078,-2.11384 12.72481,-1.00754 23.19583,-1.88256 23.26892,-1.94449 0.0731,-0.0619 7.0308,-31.82267 15.46155,-70.5794 l 15.32865,-70.466794 26.7352,-5.620213 c 14.70436,-3.091117 26.82424,-5.531179 26.93306,-5.422359 0.1684,0.168398 -46.32785,199.271566 -46.64541,199.741906 -0.0824,0.12206 -147.274425,10.5543 -149.267785,10.57935 -0.177009,0.002 -0.110856,-0.8397 0.147007,-1.87095 z" id="path43"></path></svg>';
   localStorage.setItem("color_mode", "dark");
   menu_svg.innerHTML = '<img style="vertical-align: 38%;margin-left: 20px; height:28px;" src="img/menu_white.svg">';
   document.getElementById("menu").style.background = "#242424";
@@ -48,10 +47,10 @@ function light_mode(){
   logo_img.innerHTML = '<svg version="1.1" viewBox="0 0 250 250.00001" fill="none" stroke="none" stroke-linecap="square" stroke-miterlimit="10" sodipodi:docname="u_logo_svg.svg" inkscape:version="1.3.2 (091e20e, 2023-11-25, custom)" width="250" height="250" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" style="margin-left: 10px;height: 42px;width: 42px;"><defs id="defs18"></defs><sodipodi:namedview id="namedview18" pagecolor="#ffffff" bordercolor="#000000" borderopacity="0.25" inkscape:showpageshadow="2" inkscape:pageopacity="0.0" inkscape:pagecheckerboard="0" inkscape:deskcolor="#d1d1d1" inkscape:zoom="0.50000001" inkscape:cx="95.999997" inkscape:cy="298.99999" inkscape:window-width="1920" inkscape:window-height="1129" inkscape:window-x="-8" inkscape:window-y="-8" inkscape:window-maximized="1" inkscape:current-layer="svg18"></sodipodi:namedview><clipPath id="p.0"><path d="M 0,0 H 255.29396 V 250.58792 H 0 Z" clip-rule="nonzero" id="path1"></path></clipPath><path style="fill:#0795ff;fill-opacity:1;stroke-width:0.5" d="m 29.825172,225.375 c 0.257864,-1.03125 9.533851,-42.20616 20.613305,-91.49981 11.079455,-49.293635 20.238301,-89.708141 20.352993,-89.810006 0.306169,-0.271927 51.49517,-6.946687 51.69179,-6.74032 0.0918,0.09633 -6.5622,30.437636 -14.7866,67.425136 -8.2244,36.9875 -14.837525,67.55459 -14.69584,67.92687 0.141685,0.37229 0.588255,0.55 0.99238,0.39492 0.40412,-0.15507 11.14597,-1.1063 23.87078,-2.11384 12.72481,-1.00754 23.19583,-1.88256 23.26892,-1.94449 0.0731,-0.0619 7.0308,-31.82267 15.46155,-70.5794 l 15.32865,-70.466794 26.7352,-5.620213 c 14.70436,-3.091117 26.82424,-5.531179 26.93306,-5.422359 0.1684,0.168398 -46.32785,199.271566 -46.64541,199.741906 -0.0824,0.12206 -147.274425,10.5543 -149.267785,10.57935 -0.177009,0.002 -0.110856,-0.8397 0.147007,-1.87095 z" id="path43"></path></svg>';
   localStorage.setItem("color_mode", "light");
   menu_svg.innerHTML = '<img style="vertical-align: 38%;margin-left: 20px; height:28px;" src="img/menu.svg">';
-  document.getElementById("menu").style.background = "#fff";
   const allLinks = document.querySelectorAll('a');
+  document.getElementById("menu").style.background = "#fff";
 
-// 各 a タグのスタイルを変更
+  // 各 a タグのスタイルを変更
 allLinks.forEach(link => {
   link.style.color = '#000';
 });
@@ -90,3 +89,45 @@ function close_menu()
   
   
 }
+
+
+// JavaScript
+const myElement = document.getElementById('p_menu');
+const myElement2 = document.getElementById('no_1');
+
+
+  myElement2.addEventListener('mouseover', () => {
+    // マウスが重なったときの処理
+    myElement.style.display = "block";
+
+    
+  });
+  
+
+
+  myElement.addEventListener('mouseover', () => {
+    // マウスが重なったときの処理
+    myElement.style.display = "block";
+
+    
+  });
+
+
+
+    myElement.addEventListener('mouseout', () => {
+    // マウスが重なったときの処理
+    myElement.style.display = "none";
+
+    
+  });
+
+setTimeout(() => {
+  myElement2.addEventListener('mouseout', () => {
+    // マウスが重なったときの処理
+    myElement.style.display = "none";
+
+  });
+}, 800)
+
+
+
