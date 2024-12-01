36eb2bf87fc636b5f13bec4b350f8ad60c7095c8
umaidango <162565401+umaidango@users.noreply.github.com>
2024-12-01 14:26:54 +0900

    ffff

--- main_2.js
+++ main_2.js
@@ -1,3 +1,82 @@
+const myElement = document.getElementById('p_menu');
+const myElement2 = document.getElementById('no_1');
+
+
+  myElement2.addEventListener('mouseover', () => {
+    // マウスが重なったときの処理
+    myElement.style.display = "block";
+
+    
+  });
+  
+
+
+  myElement.addEventListener('mouseover', () => {
+    // マウスが重なったときの処理
+    myElement.style.display = "block";
+
+    
+  });
+
+
+
+    myElement.addEventListener('mouseout', () => {
+    // マウスが重なったときの処理
+    myElement.style.display = "none";
+
+    
+  });
+
+setTimeout(() => {
+  myElement2.addEventListener('mouseout', () => {
+    // マウスが重なったときの処理
+    myElement.style.display = "none";
+
+  });
+}, 800)
+
+
+
+
+
+
+// JavaScript
+const myElement_0 = document.getElementById('info_menu');
+const myElement_2 = document.getElementById('no_2');
+
+
+  myElement_2.addEventListener('mouseover', () => {
+    // マウスが重なったときの処理
+    myElement_0.style.display = "block";
+
+    
+  });
+  
+
+
+  myElement_0.addEventListener('mouseover', () => {
+    // マウスが重なったときの処理
+    myElement_0.style.display = "block";
+
+    
+  });
+
+
+
+    myElement_0.addEventListener('mouseout', () => {
+    // マウスが重なったときの処理
+    myElement_0.style.display = "none";
+
+    
+  });
+
+setTimeout(() => {
+  myElement_2.addEventListener('mouseout', () => {
+    // マウスが重なったときの処理
+    myElement_0.style.display = "none";
+
+  });
+}, 800)
 
 
 // 要素を取得
