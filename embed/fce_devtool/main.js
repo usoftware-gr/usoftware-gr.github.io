b81d98f4526e57e5288630f3a4f524d21838773c
うまいだんご <ku-dango@outlook.jp>
2025-01-01 16:23:11 +0900

    ff

--- embed/fce_devtool/main.js
+++ embed/fce_devtool/main.js
@@ -3,6 +3,7 @@ window.addEventListener('scroll', () => {
     if (window.scrollY > 0) {
         header.style = "z-index: 4; background:#fff; box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 10px;   width: 130px;   border-radius: 15px;   margin: 10px;";
         header.style.animation = "0.3s updown-anime";
+        up_btn.style.display = 'block';
         logo.innerHTML = '<img src="favicon.png">';
     } else {
 
@@ -10,6 +11,7 @@ window.addEventListener('scroll', () => {
         setTimeout(() => {
             header.style = "";
             logo.innerHTML = '<img src="logo.png">';
+            up_btn.style.display = 'none';
         }, 140);
     }
   });
